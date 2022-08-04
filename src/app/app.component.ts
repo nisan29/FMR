78c0de23ee4ca853e7c38e456edc5261ab374aba
nisan1990 <nsn11ster@gmail.com>
2022-08-04 23:55:39 +0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { filter, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FMR';
+
+  ngOnInit(): void {
+    fromEvent(document, 'mouseover')
+      .pipe(filter((event: MouseEvent) =>
+        (event.target as HTMLElement).nodeName.toLowerCase() === 'img'))
+      .subscribe(s => console.log(s.target as HTMLElement));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectedImageMetadataComponent } from './components/selected-image-metadata/selected-image-metadata.component';
+import { ImagesListComponent } from './components/images-list/images-list.component';
+import { ImageViewComponent } from './components/image-view/image-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { initializeKeycloak } from './utility/app.init';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectedImageMetadataComponent,
+    ImagesListComponent,
+    ImageViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    ScrollingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
