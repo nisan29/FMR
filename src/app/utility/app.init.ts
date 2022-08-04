ddb5670e9bf4ec0c9e05cdd905b10bdf56f90ddf
nisan1990 <nsn11ster@gmail.com>
2022-08-05 00:17:15 +0300

    add KeycloakAngularModule

--- src/app/utility/app.init.ts
+++ src/app/utility/app.init.ts
@@ -4,7 +4,7 @@ export function initializeKeycloak(keycloak: KeycloakService):() => Promise<bool
   return () =>
     keycloak.init({
       config: {
-        url: 'http://localhost:8080/auth',
+        url: 'http://localhost:8080',
         realm: 'your-realm',
         clientId: 'your-client-id'
       }
